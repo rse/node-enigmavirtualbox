7ac169bc986924bd51d8be3a2b01c11405096fe9
Ralf S. Engelschall <rse@engelschall.com>
2014-09-29 18:48:25 +0200

    place initial code under version control

--- README.md
+++ README.md
@@ -1,4 +1,93 @@
-node-enigmavirtualbox
+
+Node-EnigmaVirtualBox
 =====================
 
-Node API for executing Enigma Virtual Box
+[Node](http://nodejs.org/) API for executing [Enigma Virtual Box](http://enigmaprotector.com/en/aboutvb.html).
+
+<p/>
+<img src="https://nodei.co/npm/node-enigmavirtualbox.png?downloads=true&stars=true" alt=""/>
+
+<p/>
+<img src="https://david-dm.org/rse/node-enigmavirtualbox.png" alt=""/>
+
+Abstract
+--------
+
+This is a [Node](http://nodejs.org/) API for executing the Windows
+tool [Enigma Virtual Box](http://enigmaprotector.com/en/aboutvb.html) program from within
+JavaScript. The essential point of this Node extension is not primarily
+to just abstract away the asynchronous CLI execution. Instead there
+are two other major points: First, this Node extension provides a
+fixed dependency, as other Node extensions which require Enigma Virtual Box
+under Windows can just depend (via their NPM `package.json` file) onto this extension.
+Second, as this Node extension can automatically download, locally install
+and use an Enigma Virtual Box distribution, there is no need for any previously
+available global Enigma Virtual Box installation. Just depend on this Node extension and
+Enigma Virtual Box is available!
+
+Installation
+------------
+
+Use the Node Package Manager (NPM) to install this module
+locally (default) or globally (with option `-g`):
+
+    $ npm install [-g] enigmavirtualbox
+
+Usage
+-----
+
+```js
+var EVB = require("enigmavirtualbox");
+
+EVB()
+    .config("test.evb")
+    .execute()
+    .then(function () {
+        console.log("OK: done");
+    }, function (error) {
+        console.log("ERROR: ", util.inspect(error));
+    })
+```
+
+API
+---
+
+- `EVB([options]): EVB`: constructor for the API. Call this once
+  for every generation process.
+  This returns the EVB API for further method chaining.
+
+- `EVB#config(filename): EVB`: set XML configuration file.
+  This returns the EVB API for further method chaining.
+
+- `EVB#execute(): EVB`: asynchronously execute the conversion
+  process. This returns a promise.
+
+See Also
+--------
+
+Companion Grunt task [grunt-enigmavirtualbox](https://github.com/rse/grunt-enigmavirtualbox)
+
+License
+-------
+
+Copyright (c) 2014 Ralf S. Engelschall (http://engelschall.com/)
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
---
+++ enigmavirtualbox-npm.js
@@ -0,0 +1,166 @@
+/*
+**  node-enigmavirtualbox -- Node API for executing Enigma Virtual Box
+**  Copyright (c) 2014 Ralf S. Engelschall <rse@engelschall.com>
+**
+**  Permission is hereby granted, free of charge, to any person obtaining
+**  a copy of this software and associated documentation files (the
+**  "Software"), to deal in the Software without restriction, including
+**  without limitation the rights to use, copy, modify, merge, publish,
+**  distribute, sublicense, and/or sell copies of the Software, and to
+**  permit persons to whom the Software is furnished to do so, subject to
+**  the following conditions:
+**
+**  The above copyright notice and this permission notice shall be included
+**  in all copies or substantial portions of the Software.
+**
+**  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+**  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+**  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+**  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+**  CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+**  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+**  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/* global process: false */
+/* global __dirname: false */
+/* global require: false */
+/* global console: false */
+/* eslint no-console: 0 */
+
+/*
+ *  enigmavirtualbox-npm.js: NPM install-time integration
+ */
+
+/*  core requirements  */
+var child_process = require("child_process");
+var fs            = require("fs");
+var path          = require("path");
+var zlib          = require("zlib");
+
+/*  extra requirements  */
+var progress      = require("progress");
+var promise       = require("promise");
+var request       = require("request");
+var which         = require("which");
+var chalk         = require("chalk");
+var rimraf        = require("rimraf");
+
+/*  return download URL for latest PrinceXML distribution  */
+var downloadURL = function () {
+    return "http://enigmaprotector.com/assets/files/enigmavb.exe";
+};
+
+/*  download data from URL  */
+var downloadData = function (url) {
+    return new promise(function (resolve, reject) {
+        console.log("-- download: " + url);
+        var options = {
+            method: "GET",
+            url: url,
+            encoding: null,
+            headers: {
+                "User-Agent": "node-enigmavirtualbox (enigmavirtualbox-npm.js:install)"
+            }
+        };
+        if (typeof process.env.http_proxy === "string" && process.env.http_proxy !== "")
+            options.proxy = process.env.http_proxy;
+        var req = request(options, function (error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log("-- download: " + body.length + " bytes received.");
+                resolve(body);
+            }
+            else
+                reject("download failed: " + error);
+        });
+        var progress_bar = null;
+        req.on("response", function (response) {
+            var len = parseInt(response.headers["content-length"], 10);
+            progress_bar = new progress(
+                "-- download: " +
+                "[" + chalk.blue(":bar") + "] " +
+                chalk.blue(":percent") +
+                " (ETA: " + chalk.blue(":eta") + "s)", {
+                complete:   "#",
+                incomplete: "=",
+                width:      40,
+                total:      len
+            });
+        });
+        req.on("data", function (data) {
+            if (progress_bar !== null)
+                progress_bar.tick(data.length);
+        });
+    });
+};
+
+/*  extract a tarball (*.tar.gz)  */
+var extractTarball = function (tarball, destdir, stripdirs) {
+    return new promise(function (resolve, reject) {
+        fs.createReadStream(tarball)
+            .pipe(zlib.createGunzip())
+            .pipe(tar.Extract({ path: destdir, strip: stripdirs }))
+            .on("error", function (error) { reject(error); })
+            .on("end", function () { resolve(); });
+    });
+};
+
+/*  main procedure  */
+if (process.platform !== "win32") {
+    console.log("WARNING: Enigma Virtual Box is available for Windows platform only! (this module will be void on your platform)");
+    process.exit(0);
+}
+if (process.argv.length !== 3) {
+    console.log("ERROR: invalid number of arguments");
+    process.exit(1);
+}
+var destdir;
+if (process.argv[2] === "install") {
+    /*  installation procedure  */
+    console.log("++ downloading Enigma Virtual Box distribution");
+    var url = downloadURL();
+    downloadData(url).then(function (data) {
+        console.log("++ locally unpacking Enigma Virtual Box distribution");
+        destdir = path.join(__dirname, "enigmavb");
+        var destfile = path.join(__dirname, "enigmavb.exe");
+        fs.writeFileSync(destfile, data, { encoding: null });
+        var args = [
+            "/SILENT",
+            "/VERYSILENT",
+            "/SP-",
+            "/SUPPRESSMSGBOXES",
+            "/NORESTART",
+            "/NOICONS",
+            "/DIR=\"" + path.resolve(destdir) + "\""
+        ];
+        child_process.execFile(destfile, args, function (error /*, stdout, stderr */) {
+            if (error) {
+                console.log(chalk.red("** ERROR: failed to extract: " + error));
+            }
+            else {
+                fs.unlinkSync(destfile);
+                console.log("-- OK: local Enigma Virtual Box installation now available");
+            }
+        });
+    }, function (error) {
+        console.log(chalk.red("** ERROR: failed to download: " + error));
+    });
+}
+else if (process.argv[2] === "uninstall") {
+    /*  uninstallation procedure  */
+    destdir = path.join(__dirname, "enigmavb");
+    if (fs.existsSync(destdir)) {
+        console.log("++ deleting locally unpacked Enigma Virtual Box distribution");
+        rimraf(destdir, function (error) {
+            if (error !== null)
+                console.log("** ERROR: " + error);
+            else
+                console.log("-- OK: done");
+        });
+    }
+}
+else {
+    console.log("ERROR: invalid argument");
+    process.exit(1);
+}
+
