3db6c823a07e0acc422b888dd7ca00832a30db0f
Zayin Krige <zkrige@users.noreply.github.com>
2019-03-21 09:14:49 +0200

    Update enigmavirtualbox-api.js
    The options are not being passed to child_process.execFile, so the memory change has no effect. This fixes

--- enigmavirtualbox-api.js
+++ enigmavirtualbox-api.js
@@ -50,7 +50,7 @@ var executeProg = function (prog, args, ignoreOutput) {
             var options = {};
             options.cwd = process.cwd();
             options.maxbuffer = 10 * 1024 * 1024;
-            child_process.execFile(prog, args, { cwd: process.cwd() },
+            child_process.execFile(prog, args, options,
                 function (error, stdout, stderr) {
                     if (error === null && !ignoreOutput && !stdout.match(/(?:.|[\r\n])*\[\d{2}:\d{2}:\d{2}\]/))
                         reject({ error: "unknown error (no processing information found in output)", stdout: stdout, stderr: stderr });
